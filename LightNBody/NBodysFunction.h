39fa7c5ed98576aa20d3122de45f6a93712e56e1
MaloDrougard <malcommalo.drougard@epfl.ch>
2014-11-25 15:16:16 +0100

    update particle

--- LightNBody/NBodysFunction.h
+++ LightNBody/NBodysFunction.h
@@ -9,10 +9,9 @@
 // This function take the a set of particles 
 // and compute the new position of particle after a laps of time
 
-int NBodysAttraction(std::vector<Particle> * particlesSet);
-int NBodysTravel(std::vector<Particle> * particlesSet, double time );
+int NBodysTravel(std::vector<Particle> * particlesSet, double time);
 int BarnesHutAttractions(std::vector<Particle> * set, Tree *  look, double accurancy);
-vector<double> calculateAttraction(Particle p, Tree * look, double accurancy); 
+vector<double> calculateAttraction(Particle * p, Tree * look, double accurancy); 
 
 
 const double G = 1; // 0.0000000000667259;
