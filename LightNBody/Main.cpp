5f307f207b6928f4606f89118a34e1e940274724
MaloDrougard <malcommalo.drougard@epfl.ch>
2014-12-10 20:04:47 +0100

    add parsing function Main, add laucher

--- LightNBody/Main.cpp
+++ LightNBody/Main.cpp
@@ -9,6 +9,7 @@
 #include "NBodysFunction.h"
 #include "Generator.h"
 #include "Print.h"
+#include <string.h>
 
 
 using namespace std;
@@ -18,22 +19,33 @@ char GENERATORFILE[100] = "tab1024";
 char RESULTFILE[100] = "result.txt";
 int COUNTPARTICLE = 100000;
 int NUMSLOT = 20;
-double DELTATIME = 1;
+double DELTATIME = 0.01;
 int NUMTHREADS = 1;
-bool INPUT = true;
+int ACCURACY = 0;
+bool INPUT;
 
+void setArguments(int argc, char * argv[]); // args: inFile, ouFile, numThreads, numSlot, deltaTime, accuracy
 void getInput();
 
-int main()
-{	
-	TimeAnalyzer analyzer;
+int main(int argc, char* argv[]) {
+
 	
+	setArguments(argc, argv);	
 	if (INPUT){
 		getInput();
 	}
 	
+	cout << endl;
+	cout << "INPUT_FILE: " << GENERATORFILE << endl;
+	cout << "OUTPUT_FILE: " << RESULTFILE << endl;
+	cout << "NUMBER_OF_THREADS: " << NUMTHREADS << endl;
+	cout << "NUMBER_OF_SLOTS: " << NUMSLOT << endl;
+	cout << "DELTATIME: " << DELTATIME << endl;
+	cout << "ACCURACY: " << ACCURACY << endl << endl;
+
 	/* initialization */
 
+	TimeAnalyzer analyzer;
 	vector<Particle>  set = GenerateSet(GENERATORFILE, COUNTPARTICLE);
 	NUMTHREADS = omp_get_num_threads();
 	initFile(RESULTFILE, set.size() , NUMTHREADS, NUMSLOT, DELTATIME);
@@ -58,10 +70,46 @@ int main()
 	rawTableToFile(&analyzer, RESULTFILE);
 
 	cout << "Execution time: " << analyzer.getTotalTime(0) << endl;
-	cout << "Type any character to close this program "  << endl;
-	getchar();
+	if (INPUT){
+		cout << "Type any character to close this program " << endl;
+		getchar();
+	}
+	
 
 	return 0;
+}
+
+
+void setArguments(int argc, char* argv[])
+{
+	if (argc == 1)
+	{
+		INPUT = true;
+	}
+	else if (argc == 6) // the first argument is allways the program name
+	{
+		INPUT = false;
+		strcpy(GENERATORFILE, argv[1]);
+		strcpy(RESULTFILE, argv[2]);
+		NUMTHREADS = atoi(argv[3]);
+		NUMSLOT = atoi(argv[4]);
+		DELTATIME = atof(argv[5]);
+	}
+	else if (argc == 7) 
+	{
+		INPUT = false;
+		strcpy(GENERATORFILE, argv[1]);
+		strcpy(RESULTFILE, argv[2]);
+		NUMTHREADS = atoi(argv[3]);
+		NUMSLOT = atoi(argv[4]);
+		DELTATIME = atof(argv[5]);
+		ACCURACY = atof(argv[6]);
+	}
+	else
+	{
+		cerr << "invalide number of paramters";
+		exit(3);
+	}
 }
 
 
---
+++ Results/lauch.ps1
@@ -0,0 +1,20 @@
+﻿
+echo "hello we gonna start the script"
+$method="basic"
+$infile="t"
+$outfile="l.txt"
+$num_slots=100
+$num_threads=0
+$delta_time=0.01
+$accuracy=0
+
+
+for($num_threads=1; $num_threads -le 3; $num_threads++){
+ echo "Couputation with $num_threads starts"
+ $outfile="r-$method-s$num_slots-acc$accuracy-t$num_threads.txt"
+ C:\Users\malo\Documents\"Visual Studio 2013"\Projects\NBody\Release\NBody.exe $infile $outfile $num_threads $num_slots $delta_time $accuracy
+}
+
+echo "All the computation is done"
+
+
