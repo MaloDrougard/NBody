19f68ee368dcb51788de6bada907b83b510ec0cf
MaloDrougard <malcommalo.drougard@epfl.ch>
2014-12-13 00:07:42 +0100

    modify laucher.bash

--- Results/laucher.bash
+++ Results/laucher.bash
@@ -1,7 +1,7 @@
 #!bash
 
-method="basic"
-infile="tab"
+method="basic-parallel"
+infile="tab8096"
 num_slots="100"
 delta_t="0.01"
 accuracy="0"
@@ -10,11 +10,11 @@ create_files="$method-create-files.txt"
 touch "$create_files"
 
 outfile="r.txt"
-for num_thread in $(seq 1 64)
+for num_thread in $(seq 28 64)
 do
 	echo "NBody with $num_thread is lauch" 
 	outfile="r-$method-$infile-s$num_slots-a$accuracy-t$num_thread.txt"	
-	./NBodyBasicParallel.exe $infile $outfile $num_threads $num_slots $delta_t $accuracy
+	./NBodyBasicParallel.exe $infile $outfile $num_thread $num_slots $delta_t $accuracy
 	echo $outfile >> $create_files
 
 	 
