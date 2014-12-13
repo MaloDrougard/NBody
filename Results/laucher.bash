8b4141bebeac5ba1233f925477bc47770c727d6a
MaloDrougard <malcommalo.drougard@epfl.ch>
2014-12-13 13:05:19 +0100

    after first measurment

--- Results/laucher.bash
+++ Results/laucher.bash
@@ -1,10 +1,11 @@
 #!bash
 
+
 method="tree-parallel"
 infile="tab8096"
 num_slots="100"
 delta_t="0.01"
-accuracy="0"
+accuracy="0.2"
 create_files="$method-create-files.txt"
 
 touch "$create_files"
@@ -12,16 +13,12 @@ touch "$create_files"
 outfile="r.txt"
 for num_thread in $(seq 1 64)
 do
-	echo "NBody with $num_thread is lauch" 
-	outfile="r-$method-$infile-s$num_slots-a$accuracy-t$num_thread.txt"	
-	./NBodyBasicParallel.exe $infile $outfile $num_thread $num_slots $delta_t $accuracy
-	echo $outfile >> $create_files
+        echo "NBody with $num_thread is lauch"
+        outfile="r-$method-$infile-s$num_slots-a$accuracy-t$num_thread.txt"
+        ./NBodyTreeParallel.exe $infile $outfile $num_thread $num_slots $delta_t $accuracy
+        echo $outfile >> $create_files
 
-	 
+
 done
 
 
- 
-
-
- 
