81498084a797f762516e18c3777e8d05ab20b9a4
MaloDrougard <malcommalo.drougard@epfl.ch>
2014-12-15 19:00:27 +0100

    laucher

--- Results/laucher.bash
+++ Results/laucher.bash
@@ -1,27 +1,24 @@
+
 #!bash
 
 
 method="tree-parallel"
-infile="tab8096"
-num_particles="0"
-num_slots="30"
+infile="tab65536"
+num_slots="50"
 delta_t="0.01"
-num_thread="1"
 accuracy="0.2"
-create_files="$method-create-files-countparticle.txt"
+create_files="$method-create-files.txt"
 
 touch "$create_files"
 
 outfile="r.txt"
-num_particles="253"
-while [ $num_particles -le 8096 ]
+for num_thread in $(seq 32 64)
 do
         echo "NBody with $num_thread is lauch"
-        outfile="r-$method-$infile-p$num_particles-s$num_slots-a$accuracy-t$num_thread.txt"
-        ./NBodyTreeParallelSmallCountParticles.exe $infile $outfile $num_thread $num_slots $delta_t $accuracy $num_particles
+        outfile="r-$method-$infile-s$num_slots-a$accuracy-t$num_thread.txt"
+        ./NBodyTreeParallelSmall.exe $infile $outfile $num_thread $num_slots $delta_t $accuracy
         echo $outfile >> $create_files
-	num_particles=$[$num_particles+253] 
+
 
 done
-
 
