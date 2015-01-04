2895e9df59e36566394aea328b5a8b2f4797c183
MaloDrougard <malcommalo.drougard@epfl.ch>
2015-01-04 20:11:03 +0100

    add results

--- Results/import_data.R
+++ Results/import_data.R
@@ -1,7 +1,15 @@
+# import the data from an output file from a result file of the NBody software
+# and construct the approptiate time table
+
+
+
 # i for index, s for start, e for end, n for name
 
-name = "defaut_input_file.txt"
+name = "r-basic-parallel-tab8096-p253-s30-t1.txt"
 clear = FALSE #clean or not the value and data at the end of the script
+
+# uncomment this to bound this with the plot.R
+myCommandArgs = function() NULL
 
 args <- myCommandArgs()
 if ( length(args) == 1){
@@ -13,7 +21,9 @@ text = readLines(name)
 file_header = read.table(text = text[1])
 num_threads = file_header[1,4]
 num_slots = file_header[1,8]
-  
+num_particles = file_header[1,6]
+
+
 #table name:
 total_start_n = "TOTALTIME_TABLE_START"
 total_end_n = "TOTALTIME_TABLE_END"
@@ -53,32 +63,49 @@ solo = (sapply(solo_part1_e, max) - sapply(solo_part1_s, max)) +
 
 ################# TOTAL TIME ##################################
 total_t = max(total_e) -  min(total_s)
-mean_time_per_slot = total_t / num_slots
 time_slots = para + solo
+mean_time_per_slot = mean(time_slots)
 sum = sum(para) + sum(solo)
 
 
 ################### FRACTIONS #################################
-f_solo = solo / (solo + para) 
+f_solo = solo/ (solo + para) 
 f_mean_solo =  mean(f_solo)
+mean_solo = mean(solo)
 
 ################# DIFFERENCE BETWEEN THE THREADS ##############
-dif_para = sapply( (para_e - para_s) , max ) - sapply( (para_e - para_s), min)
+dif_para = sapply( para_e, max ) - sapply( para_e, min)
 dif_mean = mean(dif_para)
 
 #################### SUMMARY ##################################
+# sequntial_time is the time where the code is executec by only one threads
+# wait_time is  the time that the thread whos has finisch first wait on the others
 
-summary = matrix(NA, 1, 3)
-colnames(summary) = c("TIME_PER_SLOT", "NONE_PARALLEL_FRACTION", "DIFFERENCE_PARALLEL_SECTION")
+summary = matrix(NA, 1, 5)
+
+colnames(summary) = c("TIME_PER_SLOT", "SEQUENTIAL_TIME", "WAIT_TIME", "NUM_PARTICLES", "SOLO_TIME_PART2")
 summary[1,1] = mean_time_per_slot
-summary[1,2] = f_mean_solo
+summary[1,2] = mean_solo
 summary[1,3] = dif_mean
+summary[1,4] = num_particles
+summary[1,5] = mean(sapply(solo_part2_e, max) - sapply(solo_part2_s, max))
 
 if(length(grep(summary_n, text, ignore.case = TRUE)) == 0){
   write("\n" , name, append = TRUE)
   write(summary_n , name, append = TRUE)
   write.table(summary, name,  append = TRUE, col.names = TRUE)
 }
+
+
+################ PLOTS #######################################
+
+#pdf= TRUE
+#if(pdf){
+#  pdf(paste(name, "slots-times.pdf", sep=""), width=6, heigh=4)
+#  plot(time_slots, xlab="slot", ylab="time", ylim = c(0,3.5))
+#  dev.off()
+#}
+
 
 
 #################### END AND CLEAR ############################
@@ -89,6 +116,7 @@ if(clear){
      total_end_n, total_start_n, para_end_n, para_start_n)
   rm(args, clear, dif_mean, dif_para, f_mean_solo,f_solo,getLineNum,getTable, 
      mean_time_per_slot, name, num_slots, num_threads,sum, summary_n, text, time_slots)   
+  rm(mean_solo, num_particles)
 }
 
 
